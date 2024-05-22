e198c4b14b248391394711df9f85d35c35ff1b3d
Abhinav Singh <abhinavas430@gmail.com>
2024-05-22 18:56:54 +0530

    refactor: Update leaderboard data fetching logic and sorting criteria

--- src/app/leaderboard/page.js
+++ src/app/leaderboard/page.js
@@ -17,23 +17,43 @@ const LeaderBoardPage = () => {
   useEffect(() => {
     const db = database;
     const usersRef = ref(db, "users");
-    const usersQuery = query(usersRef, orderByChild("score"), limitToLast(10));
+    const usersQuery = query(usersRef, orderByChild("lastAnswered"), limitToLast(10));
 
-    const fetchData = () => {
-      onValue(usersQuery, (snapshot) => {
-        const data = snapshot.val();
-        if (data) {
-          // Convert object of objects to array of objects
-          const dataArray = Object.keys(data).map((key) => ({
-            ...data[key],
-            email: key,
-          }));
-          // Sort users based on score
-          const sortedData = dataArray.sort((a, b) => b.score - a.score);
-          setLeaderboardData(sortedData);
-        }
-      });
-    };
+ const fetchData = () => {
+   onValue(usersQuery, (snapshot) => {
+     const data = snapshot.val();
+     if (data) {
+       // Convert object of objects to array of objects
+       const dataArray = Object.keys(data).map((key) => ({
+         ...data[key],
+         email: key,
+       }));
+
+       // Sort users based on score in descending order
+       const sortedByScore = dataArray.sort((a, b) => b.score - a.score);
+
+       // Find the maximum score
+       const maxScore = sortedByScore.length > 0 ? sortedByScore[0].score : 0;
+
+       // Further sort by lastAnswered (least recently answered first) if score is the same
+       const sortedData = sortedByScore.sort((a, b) => {
+         if (a.score === maxScore && b.score === maxScore) {
+           // Convert lastAnswered to milliseconds for comparison
+           const lastAnsweredA = new Date(a.lastAnswered).getTime();
+           const lastAnsweredB = new Date(b.lastAnswered).getTime();
+           return lastAnsweredA - lastAnsweredB;
+         } else {
+           // Users with different scores should remain in their sorted order
+           return 0;
+         }
+       });
+
+       setLeaderboardData(sortedData);
+     }
+   });
+ };
+
+
 
     fetchData();
 
